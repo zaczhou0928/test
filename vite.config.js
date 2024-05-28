49c326c3cf08e7e93dd6295e3e16109432175f78
Meow <qianjin@Qianjins-MBP.attlocal.net>
2024-05-27 19:40:26 -0700

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+   plugins: [react()],
++  base: '/vite-deploy-demo/'
 })
